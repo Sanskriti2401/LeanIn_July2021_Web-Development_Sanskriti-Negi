72dfdae8bb7273e5e1a6b20296fff25fe0ce72a9
sanskritinegi2401@gmail.com <sanskritinegi2401@gmail.com>
2021-08-14 01:24:10 +0530

    changes

--- Milestone3/greeting/src/App.js
+++ Milestone3/greeting/src/App.js
@@ -2,9 +2,38 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let date = new Date();
+  date = date.getHours;
+  let greet = "";
+  const cssStyle = {};
+
+  if (date > 5 && date <= 11) {
+    greet = "Good Morning!!";
+    cssStyle.color = "#FFB740";
+  } else {
+    if (date >= 12 && date <= 16) {
+      greet = "Good Afternoon!!";
+      cssStyle.color = "#B61919";
+    } else {
+      if (date >= 17 && date <= 19) {
+        greet = "Good Evening!!";
+        cssStyle.color = "#64C9CF";
+      } else {
+        greet = "Good Night!!";
+        cssStyle.color = "#7FC8A9";
+      }
+    }
+  }
+
+  const hello = () => {
+    let ans = alert("What is your name");
+  };
+
   return (
     <div className="App">
-      <h1>Hello! Sanskriti Negi</h1>
+      <h1>
+        Hello, <span style={cssStyle}>{greet}</span>
+      </h1>
     </div>
   );
 }
